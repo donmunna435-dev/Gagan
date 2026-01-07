b05e5a62553f3bc17150e88eab9a02c051444cce
donmunna435-dev <donmunna435@gmail.com>
2026-01-07 20:21:18 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config(object):
+    BOT_TOKEN = "8518891382:AAEyIGE2y1lGSkrQGE1DHalLFVPiKClvsfg "
+    API_ID = "24754824"
+    API_HASH = "e24a9c7a6aa24e1c56fa349e104ec20e"
+    ADMIN = '968292174'.split(',')
+    ADMIN_ID = [int(id) for id in ADMIN]
+    DB_URL = "mongodb+srv://n8360392852:JMXqsP0puz9qwgBe@cluster0.ce0ca.mongodb EDIT HERE  "
+    DB_NAME = "WARRIOR_EXTRACTOR"
+    TXT_LOG = -10023125
+    AUTH_LOG = -10023802
+    HIT_LOG = -100247596
+    DRM_DUMP = -10024562
+    CHANNEL = -10022752
+    CH_URL = "https://t.me/+vX1JFN-Mzhl"
+    OWNER = "https://t.me/mrcraacker"
+    THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
+    HOST = "https://drm-api-five.vercel.app"
+
